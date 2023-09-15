da1e57144f49613d19dc5e6f64063246564e775d
fatimaqais <fatima_qais@hotmail.com>
2023-09-15 23:11:13 +0000

    Filter events by searching keywords and allow infinite scroll

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -10,16 +10,20 @@ import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function EventsPage(message, filter = "") {
     const [events, setEvents] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
-        const fetchPosts = async () => {
+        const fetchEvents = async () => {
             try {
-                const { data } = await axiosReq.get(`/events/?${filter}`);
+                const { data } = await axiosReq.get(`/events/?${filter}search=${query}`);
                 setEvents(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -28,19 +32,45 @@ function EventsPage(message, filter = "") {
         };
 
         setHasLoaded(false);
-        fetchPosts();
-    }, [filter, pathname]);
+        const timer = setTimeout(() => {
+            fetchEvents();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, query, pathname]);
 
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles mobile</p>
+                <i className={`fas fa-search ${styles.SearchIcon}`} />
+                <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search posts"
+                    />
+                </Form>
+
                 {hasLoaded ? (
                     <>
                         {events.results.length ? (
-                            events.results.map((event) => (
-                                <Event key={event.id} {...event} setEvents={setEvents} />
-                            ))
+                            <InfiniteScroll
+                                children={events.results.map((event) => (
+                                    <Event key={event.id} {...event} setEvents={setEvents} />
+                                ))}
+                                dataLength={events.results.length}
+                                loader={<Asset spinner />}
+                                hasMore={!!events.next}
+                                next={() => fetchMoreData(events, setEvents)}
+                            />
                         ) : (
                             <Container className={appStyles.Content}>
                                 <Asset src={NoResults} message={message} />
