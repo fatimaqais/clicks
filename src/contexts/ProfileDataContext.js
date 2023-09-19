e5068954635c412df0b02577cba89d6a66b9fcd3
fatimaqais <fatima_qais@hotmail.com>
2023-09-19 21:55:26 +0000

    Allow the users to follow other users

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { followHelper, unfollowHelper } from "../utils/utils";
 
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
@@ -17,28 +18,79 @@ export const ProfileDataProvider = ({ children }) => {
 
     const currentUser = useCurrentUser();
 
+    const handleFollow = async (clickedProfile) => {
+        try {
+            const { data } = await axiosRes.post("/following/", {
+                followed: clickedProfile.id,
+            });
+
+            setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: {
+                    results: prevState.pageProfile.results.map((profile) =>
+                        followHelper(profile, clickedProfile, data.id)
+                    ),
+                },
+                popularProfiles: {
+                    ...prevState.popularProfiles,
+                    results: prevState.popularProfiles.results.map((profile) =>
+                        followHelper(profile, clickedProfile, data.id)
+                    ),
+                },
+            }));
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+
+    const handleUnfollow = async (clickedProfile) => {
+        try {
+            await axiosRes.delete(`/following/${clickedProfile.following_id}`);
+
+            setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: {
+                    results: prevState.pageProfile.results.map((profile) =>
+                        unfollowHelper(profile, clickedProfile)
+                    ),
+                },
+                popularProfiles: {
+                    ...prevState.popularProfiles,
+                    results: prevState.popularProfiles.results.map((profile) =>
+                        unfollowHelper(profile, clickedProfile)
+                    ),
+                },
+            }));
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+
     useEffect(() => {
         const handleMount = async () => {
             try {
                 const { data } = await axiosReq.get(
-                    "/profiles/?ordering=-followers_count"
+                    "/profiles/?ordering=-following_count"
                 );
                 setProfileData((prevState) => ({
                     ...prevState,
                     popularProfiles: data,
                 }));
             } catch (err) {
-                console.log(err);
+                // console.log(err);
             }
         };
+
         handleMount();
     }, [currentUser]);
 
     return (
         <ProfileDataContext.Provider value={profileData}>
-            <SetProfileDataContext.Provider value={setProfileData}>
+            <SetProfileDataContext.Provider
+                value={{ setProfileData, handleFollow, handleUnfollow }}
+            >
                 {children}
             </SetProfileDataContext.Provider>
         </ProfileDataContext.Provider>
-    )
+    );
 };
